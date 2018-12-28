589348b047cc82ed71d58febb7d2e8ed10a9633b
Amir Shehata <amir.shehata@gmail.com>
2018-12-28 10:20:26 -0800

    YAID: Yet Another Icon Display
    There are so many icons which can be used in labels and properties
    but there is no real good way to know what that icon looks like
    without actually using it (none that I figured out anyway). That's
    where YAID becomes useful. It's a small add-on which displays all
    available icons.
    
    Couple of issues with the addon. I don't know how to get the icon
    enumeration from the code so I can iterate through it. What I did
    is copy the icon list from the manual and am iterating over that.
    
    Ideally, we should be able to get the enumeration in the code and
    iterate over that. This way if the enumeration changes in the future
    we can simply pick it up.
    
    Signed-off-by: Amir Shehata <amir.shehata@gmail.com>

---
+++ __init__.py
@@ -0,0 +1,61 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  Copyright (C) 2018 Amir Shehata
+#  http://www.openmovie.com
+#  amir.shehata@gmail.com
+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+import bpy
+import traceback
+from .icon_list import icons
+
+bl_info = {
+    "name": "YAID",
+    "version": (0, 1),
+    "blender": (2, 80, 0),
+    "location": "View3D > UI > YAID",
+    "author": "Amir Shehata <amir.shehata@gmail.com>",
+    "description": "Yet Another Icon Display",
+    "category": "display all the icons"
+}
+
+class YAID_PT_display_icons(bpy.types.Panel):
+    bl_label = "Icon Display"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = 'YAID'
+
+    def draw(self, context):
+        scn = context.scene
+        layout = self.layout
+        row = layout.row(align=True)
+        i = 0
+        for icon in icons:
+            if i > 4:
+                row = layout.row(align=True)
+                i = 0
+            i = i + 1
+            row.label(text=icon, icon=icon)
+
+def register():
+    bpy.utils.register_class(YAID_PT_display_icons)
+
+def unregister():
+    bpy.utils.unregister_class(YAID_PT_display_icons)
+
+if __name__ == "__main__":
+    register()
